7e98a54930610dc2ceaf69a838759f5982dbe9e9
Juan Manuel <juanmanuelmd24@gmail.com>
2017-08-25 10:47:09 -0500

    Actividad 1

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
-use GuzzleHttp\Client;
-use Symfony\Component\HttpFoundation\Response;
-$api_key = getenv('OPEN_API'); 
-
-$app = new Silex\Application();
-$app->get('/clima', function() use($app,$api_key) {
-    
-$client = new Client();
-$url = 'http://api.openweathermap.org/data/2.5/weather?id=3530597&appid=$api_key&units=metric';
-$response = $client->get($url);
-$body = $response->getBody();
-return new Response($body,200,array('Content-Type' => 'application/json'));
+ 
+ $app = new Silex\Application();
+ 
+ 
+ $app->get('/clima', function() use($app){ 
+ 
+ $arreglo = ['Nombre: Juan Manuel Miranda Díaz' =>'Cuenta: 415020309'];
+     return $app->json($arreglo); 
+	 
 });
+ 
  
  $app->run();
